fa7b1429d407905799a7e630e55323808d00553f
CloudMonkeyCoding <69334632+CloudMonkeyCoding@users.noreply.github.com>
2025-08-10 20:34:29 +0800

    standardize admin headers

--- adminSide/ORDERS/ManageRefund.php
+++ adminSide/ORDERS/ManageRefund.php
@@ -23,7 +23,7 @@
           <svg class="w-6 h-6 text-black" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
             <path stroke-linecap="round" stroke-linejoin="round" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V4a2 2 0 10-4 0v1.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"/>
           </svg>
-          <img src="avatar.png" alt="User Avatar" class="h-10 w-10 rounded-full border border-gray-300" />
+          <img src="https://i.imgur.com/1Q2Z1ZL.png" alt="User Avatar" class="h-10 w-10 rounded-full border border-gray-300" />
         </div>
       </div>
 
